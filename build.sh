24bb66873e4944825956f2370e3aff2d3fb1b771
Nikita Lenyk <nikitalenyk2@gmail.com>
2026-02-09 19:59:39 +0200

    moved submodule to root

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "=== tt-wavelet build (clang-20, ${BUILD_TYPE}) ==="
 
-# Configure
 cmake -B "$CUR_DIR/build" \
     -G Ninja \
     -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
@@ -21,11 +20,9 @@ cmake -B "$CUR_DIR/build" \
     -DCMAKE_CXX_FLAGS="-std=c++20" \
     -S "$CUR_DIR"
 
-# Build
 cmake --build "$CUR_DIR/build" -j"$(nproc)"
 
-# Install python bindings
-pip3 install -e "$CUR_DIR/third-party/tt-metal/"
+pip3 install -e "$CUR_DIR/tt-metal/"
 
-export TT_METAL_HOME="$CUR_DIR/third-party/tt-metal"
+export TT_METAL_HOME="$CUR_DIR/tt-metal"
 echo "=== Done. TT_METAL_HOME=$TT_METAL_HOME ==="
