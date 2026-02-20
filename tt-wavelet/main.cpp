a72b9f5ad587721788120c04a340588d427b8f5d
Nikita Lenyk <nikitalenyk2@gmail.com>
2026-02-20 21:13:27 +0200

    ci: fix clang-tidy include paths

--- tt-wavelet/main.cpp
+++ tt-wavelet/main.cpp
@@ -3,7 +3,7 @@
 #include "tt-metalium/host_api.hpp"
 
 int main() {
-    std::cout << "library linked" << std::endl;
+    std::cout << "library linked\n";
     tt::tt_metal::Program program = tt::tt_metal::CreateProgram();
 
     return 0;
