d2e444135a278f6c258a60525420b1b2d6ed7f3d
Nikita Lenyk <nikitalenyk2@gmail.com>
2026-02-24 11:28:48 +0200

    build!: fixed build.sh to work with any order of params

--- build.sh
+++ build.sh
@@ -5,9 +5,41 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090
 source "$SCRIPT_DIR/scripts/common.sh"
 
-BUILD_TYPE=${1:-Release}
-# Parallel jobs; default to nproc if not provided
-JOBS=${2:-$(nproc)}
+BUILD_TYPE="Release"
+JOBS="$(nproc)"
+
+is_integer() {
+  [[ "$1" =~ ^[0-9]+$ ]]
+}
+
+case $# in
+  0)
+    ;;
+  1)
+    if is_integer "$1"; then
+      JOBS="$1"
+    else
+      BUILD_TYPE="$1"
+    fi
+    ;;
+  2)
+    if is_integer "$1" && ! is_integer "$2"; then
+      JOBS="$1"
+      BUILD_TYPE="$2"
+    elif ! is_integer "$1" && is_integer "$2"; then
+      BUILD_TYPE="$1"
+      JOBS="$2"
+    else
+      log ERROR "With 2 arguments pass exactly one number (JOBS) and one string (BUILD_TYPE)." >&2
+      log ERROR "Usage: $0 [BUILD_TYPE|JOBS] [BUILD_TYPE|JOBS]" >&2
+      exit 1
+    fi
+    ;;
+  *)
+    log ERROR "Usage: $0 [BUILD_TYPE|JOBS] [BUILD_TYPE|JOBS]" >&2
+    exit 1
+    ;;
+esac
 
 log INFO "Starting full build (tt-metal + tt-wavelet) with -j${JOBS}"
 export_tt_env
