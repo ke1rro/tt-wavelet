a2bb8cda0506bd79f1fb94069b093564bdff6623
Nikita Lenyk <nikitalenyk2@gmail.com>
2026-03-09 20:13:30 +0200

    fix(main): fixed compute path

--- tt-wavelet/main.cpp
+++ tt-wavelet/main.cpp
@@ -120,7 +120,7 @@ int main() {
 
     tt::tt_metal::KernelHandle eltwise_sfpu_kernel_id = CreateKernel(
         program,
-        "eltwise_sfpu/kernels/compute/eltwise_sfpu.cpp",
+        "eltwise_sfpu/kernels/compute/compute.cpp",
         core,
         tt::tt_metal::ComputeConfig{
             .math_approx_mode = false,
