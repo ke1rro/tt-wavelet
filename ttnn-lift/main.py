559eaf26f6780247b42526570337b035d2ffdf55
Nikita Lenyk <nikitalenyk2@gmail.com>
2026-03-14 16:54:09 +0200

    ttnn-wip(lift): added forward lift step to be compared to ttnn

--- ttnn-lift/main.py
+++ ttnn-lift/main.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "tt-metal", "ttnn"))
+
 import torch
 import ttnn
 from tt_wavelet.lifting import BoundaryModes, LiftingWaveletTransform, load_lifting_scheme
