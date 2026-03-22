3237839df5ccd30567c9fe441066dc07677fd818
Nikita Lenyk <nikitalenyk2@gmail.com>
2026-03-22 23:46:48 +0200

    wip: added sfpi mac face [skip ci]

--- tt-wavelet/ckernels/sfpu/ckernel_sfpu_stencil_mac.h
+++ tt-wavelet/ckernels/sfpu/ckernel_sfpu_stencil_mac.h
@@ -2,20 +2,27 @@
 
 #include <cstdint>
 
+#include "ckernel_defs.h"
+#include "sfpi.h"
+
 namespace ckernel::sfpu {
 
-template <typename... Args>
+template <typename CoeffAccessor>
 inline void calculate_stencil_mac_face(
     std::uint32_t dst_index_base,
     const std::uint32_t* dst_input_indices,
-    std::uint32_t filter_len,
+    const std::uint32_t filter_len,
     std::uint32_t dst_index_out,
-    Args&&... args) {
-    (void)dst_index_base;
-    (void)dst_input_indices;
-    (void)filter_len;
-    (void)dst_index_out;
-    (void)sizeof...(args);
+    const CoeffAccessor& coeffs) {
+    vFloat result = dst_reg[dst_index_base];
+
+    for (std::uint32_t i{0}; i < filter_len; ++i) {
+        vFloat input{dst_reg[dst_input_indices[i]]};
+        vFloat coeff{coeffs[i]};
+        result = result + coeff * input;
+    }
+
+    dst_reg[dst_index_out] = result;
 }
 
 }  // namespace ckernel::sfpu
